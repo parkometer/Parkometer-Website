cca5a5e6e2be4bc9e8ee6bee390bdd9cd93640bd
ssrossel <ssrossel@gmail.com>
2019-11-25 17:22:11 +0100

    adding CMP

--- Source/js/main.js
+++ Source/js/main.js
@@ -1,9 +1,44 @@
-$("#arrow-button").click(function(){
-    $("#arrow-button-icon").removeClass("bounce");
-    $('html, body').animate({
-        scrollTop: $("#usp-container").offset().top
-    }, 900);
-});
+function toggleModal() {
+    document.onkeydown = (e) => {
+        if (e.code === 'Escape' && !document.querySelector('.modal').classList.contains('hidden')) {
+            document.querySelector('.modal').classList.toggle('hidden');
+        }
+    };
+    document.querySelector('.modal').classList.toggle('hidden');
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function denyConsent() {
+    setCookie('hc','false', 365);
+    toggleModal();
+}
+
+function acceptConsent() {
+    setCookie('hc', 'true',-1);
+    toggleModal();
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 
 
